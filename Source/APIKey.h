5053ee3e2c8b6d9bb1afb882f062375ecf3300fc
Maxime Perreault <maxime.perreault@gmail.com>
2014-10-21 16:47:42 -0500

    start of calendar view

---
+++ Source/APIKey.h
@@ -0,0 +1,7 @@
+
+#warning To use GooglePlacesAutocomplete, please register an APIKey for your application and set it here.
+
+#define kGoogleAPIKey @"AIzaSyCNQwkc4EmiEpJX9nI8m6oUthrMjfuRUkM"
+
+// DO NOT CHECK IN CHANGES TO THIS FILE.
+// Your API Key is private. To tell git to ignore local changes to this file,
